84744a7d395ca2886be6dc0c3dcd62891d07fafc
Grames Gernot <gernot.grames@gmx.at>
2020-09-22 09:56:53 +0200

    update

--- build.sh
+++ build.sh
@@ -1,25 +1,29 @@
-#!/bin/bash
+    #!/bin/bash
 
-PREFIX="grames.ddns.net"
-VERSION=2.256
+    PREFIX="myname"
+    VERSION=2.256
+    IMAGE="jenkins-arm"
 
-BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-RESOURCEDIR="$BASEDIR/resources"
+    BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+    RESOURCEDIR="$BASEDIR/resources"
 
-if [ ! -d "$RESOURCEDIR" ]; then
-  mkdir $RESOURCEDIR && cd $RESOURCEDIR
-  curl -sSLk -o tini https://github.com/krallin/tini/releases/download/v0.13.0/tini-static-armhf
-  curl -sSLO https://raw.githubusercontent.com/jenkinsci/docker/master/init.groovy
-  curl -sSLO https://raw.githubusercontent.com/jenkinsci/docker/master/jenkins.sh
-  curl -sSLO https://raw.githubusercontent.com/jenkinsci/docker/master/install-plugins.sh
-  curl -sSLO https://raw.githubusercontent.com/jenkinsci/docker/master/plugins.sh
-  curl -sSLO https://raw.githubusercontent.com/jenkinsci/docker/master/jenkins-support
-  chmod +rx *
-  cd $BASEDIR
-fi
+    if [ ! -d "$RESOURCEDIR" ]; then
+      mkdir $RESOURCEDIR && cd $RESOURCEDIR
+      curl -sSLk -o tini https://github.com/krallin/tini/releases/download/v0.19.0/tini-static-armhf
+      curl -sSLO https://raw.githubusercontent.com/jenkinsci/docker/master/tini-shim.sh
+      curl -sSLO https://raw.githubusercontent.com/jenkinsci/docker/master/jenkins.sh
+      curl -sSLO https://raw.githubusercontent.com/jenkinsci/docker/master/install-plugins.sh
+      curl -sSLO https://raw.githubusercontent.com/jenkinsci/docker/master/plugins.sh
+      curl -sSLO https://raw.githubusercontent.com/jenkinsci/docker/master/jenkins-support
+      curl -sSLO https://raw.githubusercontent.com/jenkinsci/docker/master/jenkins-plugin-cli.sh
+      chmod +rx *
+      cd $BASEDIR
+    fi
 
-docker build --rm --no-cache -t $PREFIX/jenkins-arm:$VERSION .
-docker tag $PREFIX/jenkins-arm:$VERSION $PREFIX/jenkins-arm:latest
+    docker build -t $PREFIX/$IMAGE:$VERSION .
+    docker tag $PREFIX/$IMAGE:$VERSION $PREFIX/$IMAGE:latest 
+    docker tag $PREFIX/$IMAGE:$VERSION docker.io/ggrames/$IMAGE:$VERSION
 
-docker push $PREFIX/jenkins-arm:$VERSION
-docker push $PREFIX/jenkins-arm:latest
+    docker push $PREFIX/$IMAGE:$VERSION
+    docker push $PREFIX/$IMAGE:latest
+    docker push docker.io/ggrames/$IMAGE:$VERSION
